97de05b244705dc7bbe97bfa7414332cfa4886d8
Evan Pearson <parsnbl@icloud.com>
2023-11-29 14:16:18 -0800

    cleared tree

--- wavejs/fileController.js
+++ wavejs/fileController.js
@@ -1,75 +1,70 @@
 const path = require('node:path');
 const fs = require('node:fs');
 const crypto = require('crypto');
-const { error, info } = require('./logger');
+// const { error, info } = require('./logger');
 
 
 
-const FileController = (Id) => {
-  let streamId = Id;
-  return {
+class FileController {
+  constructor(streamId, mediaRoot = '../videoFiles') {
+    this._mediaRoot = mediaRoot;
+    this._streamId = streamId;
+  }
     /* STREAM IDs */
-    setStreamId(newId) {
-      streamId = newId
-    },
-    getStreamId() {
-      return streamId;
-    },
-    generateStreamId() {
-      const streamId = crypto.randomUUID();
-      info(`Your stream id is ${streamId}.`);
-      return streamId;
-    },
-    /* HLS Methods*/
-    buildHLSDirPath() {
-      return path.join(__dirname, '../videoFiles', 'hls', streamId);
-    },
-    buildHLSPlaylistPath() {
-      path.join(this.buildHLSDirPath(streamId), 'manifest.m3u8');
-    },
-    buildHLSDir() {
-      const path = this.buildHLSDirPath();
-      fs.mkdir(path, { recursive: true }, (err) => {
-        if (err) throw err;
-      });
-    },
-    deleteHLSDir() {
-      const path = this.buildHLSDirPath(streamId);
-      fs.rmdir(path, {recursive: true}, (err) => {
-        if (err) error('An error occurred while deleting the HLS directory.');
-        else info('HLS directory successfully deleted.');
-      })
-    },
+  setStreamId(newId) {
+    this._streamId = newId
+  }
+  getStreamId() {
+    return this._streamId;
+  }
+  generateStreamId() {
+    const streamId = crypto.randomUUID();
+    // info(`Your stream id is ${streamId}.`);
+    return streamId;
+  }
+  /* HLS Methods*/
+  buildHLSDirPath() {
+    return path.join(__dirname, this._mediaRoot, 'hls', this._streamId);
+  }
+  buildHLSPlaylistPath() {
+    path.join(this.buildHLSDirPath(this._streamId), 'manifest.m3u8');
+  }
+  buildHLSDir() {
+    const path = this.buildHLSDirPath();
+    fs.mkdir(path, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+  }
+  deleteHLSDir() {
+    const path = this.buildHLSDirPath(this._streamId);
+    fs.rmdir(path, {recursive: true}, (err) => {
+      // if (err) error('An error occurred while deleting the HLS directory.');
+      // else info('HLS directory successfully deleted.');
+    })
+  }
 
-    /* MPD Method */
-    buildMPDDirPath() {
-      return path.join(__dirname, '../videoFiles', 'mpd', streamId);
-    },
-    buildMPDPlaylistPath() {
-      path.join(this.buildMPDDirPath(streamId), 'manifest.mpd');
-    },
-    buildMPDDir() {
-      const path = this.buildMPDDirPath();
-      fs.mkdir(path, { recursive: true }, (err) => {
-        if (err) throw err;
-      });
-    },
-    deleteMPDDir() {
-      const path = this.buildMPDDirPath(streamId);
-      fs.rmdir(path, {recursive: true}, (err) => {
-        if (err) error('An error occurred while deleting the HLS directory.');
-        else info('HLS directory successfully deleted.');
-      })
-    },
-  };
+  /* MPD Method */
+  buildMPDDirPath() {
+    return path.join(__dirname, this._mediaRoot, 'mpd', this._streamId);
+  }
+  buildMPDPlaylistPath() {
+    path.join(this.buildMPDDirPath(this._streamId), 'manifest.mpd');
+  }
+  buildMPDDir() {
+    const path = this.buildMPDDirPath();
+    fs.mkdir(path, { recursive: true }, (err) => {
+      if (err) throw err;
+    });
+  }
+  deleteMPDDir() {
+    const path = this.buildMPDDirPath(this._streamId);
+    fs.rmdir(path, {recursive: true}, (err) => {
+      // if (err) error('An error occurred while deleting the HLS directory.');
+      // else info('HLS directory successfully deleted.');
+    })
+  }
 };
 
+const test = new FileController('test')
 
-module.exports = {
-  generateStreamId,
-  buildHLSPlaylistPath,
-  buildHLSDirPath,
-  buildHLSDir,
-  buildHLSSegmentPath,
-  deleteHLSDir
-};
+module.exports = FileController;
