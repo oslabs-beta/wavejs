c1df34c3c36d15fa6f89b0dc82801e9f73239d7d
Sean Kirkpatrick <kirkpatrick.sk@gmail.com>
2023-11-29 14:01:56 -0800

    pre dev merge commit

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,13 +1,39 @@
+const App = require("./App.js").default;
 const request = require('supertest');
 
+
+const port = 8000;
+
 const server = 'http://localhost:8000';
+
+describe('Express App', () => {
+  describe('GET /', () => {
+    test('should return status 200 and serve the HTML file', async () => {
+      const response = await request(App).get('/');
+      expect(response.status).toBe(200);
+      expect(response.type).toBe('text/html');
+    });
+  });
+
+
+  beforeAll(() => {
+    App.listen(port, () => {
+      console.log('Listening on port', port);
+    });
+  });
+
+
+  afterAll((done) => {
+    App.close(() => {
+      done();
+    });
+  });
+});
+
 
 describe('Route integration', () => {
     describe('/', () => {
       describe('GET', () => {
-        // Note that we return the evaluation of `request` here! It evaluates to
-        // a promise, so Jest knows not to say this test passes until that
-        // promise resolves. See https://jestjs.io/docs/en/asynchronous
         it('responds with 200 status and text/html content type', () => {
           return request(server)
             .get('/')
