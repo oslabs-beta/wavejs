27423884c866ecbd5a54e7ad7a46bbd8808625b5
Evan Pearson <parsnbl@icloud.com>
2023-11-30 15:09:16 -0800

    added frontend browser conditional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import ShakaPlayer from 'shaka-player-react'
-// import 'shaka-player/dist/controls.css'
-//<iframe width="560" height="315" src="https://www.youtube.com/embed/qWNQUvIk954?si=JXgJJpEGUwkYjDWC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen style={{display:"block", margin:"auto"}}></iframe>
+import React from 'react';
+import ShakaPlayer from 'shaka-player-react';
+import { browserName, browserVersion } from "react-device-detect";;
+
+
 export default function App() {
+
     return (
       <div>
         <ReactHeader />
         <ReactBody />
-        {/* <VideoDisplay /> */}
-        <ShakaPlayer autoPlay src="http://localhost:3000/video/test/manifest.mpd" crossorigin="anonymous" />
+        {browserName === 'Safari'?
+        <VideoDisplay /> :
+        <ShakaPlayer autoPlay src="http://localhost:3000/video/test/manifest.mpd" crossorigin="anonymous" />}
       </div>
     );
   }
@@ -35,7 +38,7 @@ export default function App() {
         marginLeft:"auto", 
         marginRight:"auto"
         }}>
-          {/* <source src="http://localhost:3000/video/test/manifest.m3u8" type="application/x-mpegURL" /> */}
+          <source src="http://localhost:3000/video/test/manifest.m3u8" type="application/x-mpegURL" />
           {/* <source src="https://archive.org/download/ElephantsDream/ed_hd.ogv" type="video/ogg" /> */}
         </video>
     )
